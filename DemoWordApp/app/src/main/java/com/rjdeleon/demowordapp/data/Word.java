6d9e3ed981b6193c2c3e2cdd8084b21faf86a33b
pabs <pabs@alink-group.co.jp>
2018-11-21 06:56:27 +0800

    Add basic Room database

---
+++ DemoWordApp/app/src/main/java/com/rjdeleon/demowordapp/data/Word.java
@@ -0,0 +1,34 @@
+package com.rjdeleon.demowordapp.data;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getWord() {
+        return mWord;
+    }
+
+    public void setWord(@NonNull String word) {
+        mWord = word;
+    }
+}
---
+++ DemoWordApp/app/src/main/java/com/rjdeleon/demowordapp/data/WordRoomDatabase.java
@@ -0,0 +1,28 @@
+package com.rjdeleon.demowordapp.data;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Word.class}, version = 1)
+public abstract class WordRoomDatabase extends RoomDatabase {
+
+    public abstract WordDao wordDao();
+
+    private static WordRoomDatabase INSTANCE;
+
+    public static WordRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
