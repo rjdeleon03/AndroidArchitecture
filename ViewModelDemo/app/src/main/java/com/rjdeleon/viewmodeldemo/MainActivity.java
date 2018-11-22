c8d04e8a8f09d0797ac873b503abea046a86e82f
pabs <pabs@alink-group.co.jp>
2018-11-22 08:51:23 +0800

    Add tutorial source

--- ViewModelDemo/app/src/main/java/com/rjdeleon/viewmodeldemo/MainActivity.java
+++ ViewModelDemo/app/src/main/java/com/rjdeleon/viewmodeldemo/MainActivity.java
@@ -3,6 +3,11 @@ package com.rjdeleon.viewmodeldemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Tutorial source:
+ * https://android.jlelse.eu/using-viewmodel-android-jetpack-2a6325c79c59
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
